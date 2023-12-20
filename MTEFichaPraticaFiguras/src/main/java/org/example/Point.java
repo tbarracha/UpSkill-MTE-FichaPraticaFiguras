d7a6a7e3708a9d70c3ea61bdb219deeba8987cf3
Fábio Goncalves <fabioogoncalves@hotmail.com>
2023-12-20 15:12:11 +0000

    add classe Point Class

---
+++ MTEFichaPraticaFiguras/src/main/java/org/example/Point.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Point {
+    private double x;
+    private double y;
+
+    // Construtores
+    public Point() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Métodos Getter e Setter
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
